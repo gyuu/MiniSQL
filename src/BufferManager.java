d627c57e55681e54a21cad8dd46ebfcec5dcf3ba
gyuu <gyuu.fuu@gmail.com>
2015-11-04 21:18:44 +0800

    fix typo

--- src/BufferManager.java
+++ src/BufferManager.java
@@ -1,8 +1,6 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.sql.SQLSyntaxErrorException;
 
 import static java.lang.System.*;
 
